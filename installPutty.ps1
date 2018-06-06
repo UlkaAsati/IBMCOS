11a0a1649fcc55d501c6108a0c1899004cdc20af
UlkaAsati <ulka_asati@persistent.co.in>
2018-06-06 19:25:02 +0530

    given path explicitly

--- installPutty.ps1
+++ installPutty.ps1
@@ -1,3 +1,4 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 wget https://github.com/UlkaAsati/IBMCOS/blob/master/putty.exe?raw=true -outfile "C:\putty.exe"
-C:\putty.exe
+cd C:\
+.\putty.exe
