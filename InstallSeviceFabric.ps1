c5f3c6d69404dbe55dc332fc99de29822e43476c
UlkaAsati <ulka_asati@persistent.co.in>
2018-06-08 11:18:36 +0530

    removed extra charachter

--- InstallSeviceFabric.ps1
+++ InstallSeviceFabric.ps1
@@ -1,2 +1,3 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-wget https://github.com/UlkaAsati/IBMCOS/blob/master/MicrosoftAzure-ServiceFabric-CoreSDK.exe?raw=true -outfile "C:\MicrosoftAzure-ServiceFabric-CoreSDK.exe" -UseBasicParsing; Start-Process "C:\MicrosoftAzure-ServiceFabric-CoreSDK.exe" -ArgumentList '/AcceptEULA', '/QUIET' -NoNewWindow -Wait; \
+wget https://github.com/UlkaAsati/IBMCOS/blob/master/MicrosoftAzure-ServiceFabric-CoreSDK.exe?raw=true -outfile "C:\MicrosoftAzure-ServiceFabric-CoreSDK.exe" -UseBasicParsing; 
+Start-Process "C:\MicrosoftAzure-ServiceFabric-CoreSDK.exe" -ArgumentList '/AcceptEULA', '/QUIET' -NoNewWindow -Wait;
