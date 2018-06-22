c0334e839ad4b0e3c5764adada40f5def7f4e301
UlkaAsati <ulka_asati@persistent.co.in>
2018-06-22 17:23:38 +0530

    deleted

--- InstallSeviceFabric.ps1
+++
@@ -1,3 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-wget https://github.com/UlkaAsati/IBMCOS/blob/master/MicrosoftAzure-ServiceFabric-CoreSDK.exe?raw=true -outfile "C:\MicrosoftAzure-ServiceFabric-CoreSDK.exe" -UseBasicParsing; 
-Start-Process "C:\MicrosoftAzure-ServiceFabric-CoreSDK.exe" -ArgumentList '/AcceptEULA', '/QUIET' -NoNewWindow -Wait;
